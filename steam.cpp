2e5989f1611568fb6b47ab67ded18ca01bd4e7bf
InvoxiPlayGames <webmaster@invoxiplaygames.uk>
2025-03-31 04:21:42 +0100

    1.13 steam friends list

--- steam.cpp
+++ steam.cpp
@@ -136,7 +136,7 @@ void steam_dispatch_every_frame()
 	if (g_needsUpdatePrompt)
 	{
 		g_needsUpdatePrompt = false;
-		Com_Error(ERROR_UI, "A new update has been installed for the Windows Store Fix. Please restart the game to finish installing changes.");
+		Com_Error(ERROR_UI, "A new update has been installed for BO3Enhanced. Please restart the game to finish installing changes.");
 	}
 }
 
@@ -590,18 +590,97 @@ typedef struct _FriendsList {
 	FriendsListSorting sorting;
 } FriendsList;
 
+// https://stackoverflow.com/a/28311607
+static bool is_valid_utf8(const char* string)
+{
+	if (string == NULL)
+		return false;
+	const unsigned char* bytes = (const unsigned char*)string;
+	unsigned int cp;
+	int num;
+	while (*bytes != 0x00)
+	{
+		if ((*bytes & 0x80) == 0x00)
+		{
+			// U+0000 to U+007F 
+			cp = (*bytes & 0x7F);
+			num = 1;
+		}
+		else if ((*bytes & 0xE0) == 0xC0)
+		{
+			// U+0080 to U+07FF 
+			cp = (*bytes & 0x1F);
+			num = 2;
+		}
+		else if ((*bytes & 0xF0) == 0xE0)
+		{
+			// U+0800 to U+FFFF 
+			cp = (*bytes & 0x0F);
+			num = 3;
+		}
+		else if ((*bytes & 0xF8) == 0xF0)
+		{
+			// U+10000 to U+10FFFF 
+			cp = (*bytes & 0x07);
+			num = 4;
+		}
+		else
+			return false;
+
+		bytes += 1;
+		for (int i = 1; i < num; ++i)
+		{
+			if ((*bytes & 0xC0) != 0x80)
+				return false;
+			cp = (cp << 6) | (*bytes & 0x3F);
+			bytes += 1;
+		}
+
+		if ((cp > 0x10FFFF) ||
+			((cp >= 0xD800) && (cp <= 0xDFFF)) ||
+			((cp <= 0x007F) && (num != 1)) ||
+			((cp >= 0x0080) && (cp <= 0x07FF) && (num != 2)) ||
+			((cp >= 0x0800) && (cp <= 0xFFFF) && (num != 3)) ||
+			((cp >= 0x10000) && (cp <= 0x1FFFFF) && (num != 4)))
+			return false;
+	}
+
+	return true;
+}
+
+static void validate_and_copy_name(const char* name, char* output)
+{
+	bool invalid = false;
+	size_t namelen = strlen(name);
+	// make sure it's not too short/too long
+	if (namelen < 2 || namelen > 16)
+		invalid = true;
+	// make sure it's valid UTF8
+	if (!is_valid_utf8(name))
+		invalid = true;
+
+	// check for invalid characters
+	for (int i = 0; i < namelen; i++)
+		if (name[i] == '^' || name[i] == '@' || name[i] == '{' || name[i] == '$')
+			invalid = true;
+
+	// if the name isn't valid then they're an unknown soldier
+	if (invalid == true)
+		strncpy(output, "Unknown Soldier", 0x11);
+	else
+	{
+		strncpy(output, name, 0x10);
+		output[0x10] = 0; // null terminate
+	}
+}
+
 static void fill_in_friend(CSteamID steamID, FriendInfo* info)
 {
 	memset(info, 0, sizeof(info));
 	uint64_t uintsteam = steamID.ConvertToUint64();
 
-	// keep the name handy for parsing
-	char tempName[0x80];
-	strncpy(tempName, SteamFriends()->GetFriendPersonaName(steamID), sizeof(tempName));
-
-	// fill the name and steamid in the friendinfo struct
-	strncpy(info->name, tempName, 0x10);
-	info->name[0x10] = 0;
+	// copy the player name and XUID
+	validate_and_copy_name(SteamFriends()->GetFriendPersonaName(steamID), info->name);
 	info->presence.XUID = steamID.ConvertToUint64();
 
 	*(int*)info->presence.unk_ = time(NULL);
